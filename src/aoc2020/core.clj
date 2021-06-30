905c317c50437198efaf5005127f423f81b03f0a
Brent Walter <Brent.Walter@gmail.com>
2021-06-30 19:44:14 -0400

    More Euler stuff.

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -1,5 +1,7 @@
 (ns aoc2020.core
-  (:require [clojure.string :as str])
+  {:clj-kondo/config '{:linters {:unresolved-symbol {:exclude [FLOOR]}}}}
+  (:require [clojure.string :as str]
+            [clojure.math.numeric-tower :as math])
   (:gen-class))
 
 
@@ -39,12 +41,46 @@
 (defn euler-2
   "Find the sum of all even Fibonacci numbers < 4 million "
   []
-  (let [target 4000000]
+  (let [limit 4000000]
     (loop [f1 1 f2 1 sum-of-evens 0]
-      (if (<= f2 target)
+      (if (<= f2 limit)
         (let [f3 (+ f1 f2)
               sum (if (even? f3) (+ sum-of-evens f3) sum-of-evens)]
           (recur f2 f3 sum))
         sum-of-evens))))
 
 (euler-2)
+
+(defn fib
+  "Using Binet's formula"
+  [n]
+  (let [sqrt-5 (math/sqrt 5)
+        f #(/
+             (-' (math/expt (/ (+' 1 sqrt-5) 2) %)
+                 (math/expt (/ (-' 1 sqrt-5) 2) %))
+             sqrt-5)]
+    (biginteger
+      (with-precision 0 :rounding FLOOR (f n)))))
+
+(transduce (map fib) + (range 0N 1000 3))
+
+(map fib (range 0 51 3))
+
+(keyword "1")
+;; => (0
+;;     2
+;;     8
+;;     34
+;;     144
+;;     610
+;;     2584
+;;     10946
+;;     46368
+;;     196418
+;;     832040
+;;     3524578
+;;     14930352
+;;     63245986
+;;     267914296
+;;     1134903170
+;;     4807526976);; => 4807526976
