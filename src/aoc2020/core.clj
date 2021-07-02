41f927b64e1a0b6a44e8c95cc8a60f9f15ac7ee1
Brent Walter <brent.walter@gmail.com>
2021-07-02 12:36:36 -0400

    Versions of euler 2

--- src/aoc2020/core.clj
+++ src/aoc2020/core.clj
@@ -42,14 +42,14 @@
   "Find the sum of all even Fibonacci numbers < 4 million "
   []
   (let [limit 4000000]
-    (loop [f1 1 f2 1 sum-of-evens 0]
+    (loop [f1 1N f2 1N sum-of-evens 0]
       (if (<= f2 limit)
         (let [f3 (+ f1 f2)
               sum (if (even? f3) (+ sum-of-evens f3) sum-of-evens)]
           (recur f2 f3 sum))
         sum-of-evens))))
 
-(euler-2)
+(time (euler-2))
 
 (defn fib
   "Using Binet's formula"
@@ -58,15 +58,37 @@
         f #(/
              (-' (math/expt (/ (+' 1 sqrt-5) 2) %)
                  (math/expt (/ (-' 1 sqrt-5) 2) %))
-             sqrt-5)]
+             sqrt-5)
+        r (f n)]
     (biginteger
-      (with-precision 0 :rounding FLOOR (f n)))))
+      (with-precision 0 :rounding FLOOR r))))
 
-(transduce (map fib) + (range 0N 1000 3))
+(require '[clojure.core.reducers :as r])
 
-(map fib (range 0 51 3))
+(map #(dotimes [n 10] %)
+[
+(time (euler-2))
+(time (transduce
+  (comp (map fib)
+        (filter #(< % 4000000)))
+  + (range 0 34 3)))
+
+(time (r/fold + (r/filter #(< % 4000000) (r/map fib (range 0 34 3)))))
+
+(time (reduce + (filter #(< % 4000000) (map fib (range 0 34 3)))))
+ ])
+
+(fib 0)
+
+(range 0 34 3)
 
 (keyword "1")
+
+(map #(dotimes [n 10] %))
+
+
+
+4M
 ;; => (0
 ;;     2
 ;;     8
